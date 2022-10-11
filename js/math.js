875838fe2199fa585bee6f7959ead929f69dfa71
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-11 08:35:36 +0200

    Javascript numbers

---
+++ js/math.js
@@ -0,0 +1,34 @@
+// atvaizduoja daugiau klaidu.
+'use strict';
+console.log('math.js');
+
+let num1 = 11.458721;
+
+// Math.roud()
+let rounded = Math.round(num1);
+console.log('rounded', rounded);
+
+// floor
+let floored = Math.floor(num1);
+console.log('floored', floored);
+
+// ceil
+let ceiled = Math.ceil(num1);
+console.log('ceiled', ceiled);
+
+// toFixed
+let toFixeded = num1.toFixed(3);
+console.log('toFixeded', toFixeded);
+
+
+let num24=24;
+console.log(num24);
+
+// surasti kaip is 24 istraukti kvadratine sakni (Math)
+let sqrted = Math.sqrt(num24);
+console.log('sqrted', sqrted);
+
+
+// pakelti 5, 7 laipsniu
+let num5T7 = 5**7;
+console.log('5T7', num5T7);
