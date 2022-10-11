e1ae36e47fe4896c14ebbfa5f88e93a0d1475c64
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-11 11:56:48 +0200

    nepavykes ivedimas

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 'use strict'
 
-
-
-
-const userInfoOutput = document.getElementById('main');
-
-
-
 // const divEl = document.getElementById('result');
 // divEl.textContent = 547;
 
@@ -24,31 +17,71 @@ const userInfoOutput = document.getElementById('main');
 
 
 
-const user = {
-    vardas: "Vardenis",
-    pavarde: "Pavardenis",
 
-  };
+// const userInfoOutput = document.getElementById('main');
 
-console.log(user.vardas);
-console.log(user.pavarde);
+// const vardas = document.getElementById('vardas');
+// const pavarde = document.getElementById('pavarde');
+// const ivesti = document.getElementById('ivesti');
 
 
+// function ivesti() {
+//     const vardas = document.getElementById('vardas');
+//     const pavarde = document.getElementById('pavarde');
+// }
 
- let printText= `<h2>User information</h2>
- <p> Vartotojo vardas yra ${user.vardas}, o pavarde ${user.pavarde}</p>`
-
-console.log(printText);
 
 
-main.innerHTML=printText;
+// console.log(user.vardas);
+// console.log(user.pavarde);
+
+
+
+//  let printText= `<h2>User information</h2>
+//  <p> Vartotojo vardas yra ${user.vardas}, o pavarde ${user.pavarde}</p>`
+
+// console.log(printText);
+
+
+// main.innerHTML=printText;
 
 
 
 // Nuskaito ivedus----------------------------------------------------------------
 
-const vardas = document.getElementById('vardas');
-const pavarde = document.getElementById('pavarde');
+// const vardas = document.getElementById('vardas');
+// const pavarde = document.getElementById('pavarde');
 
+
+
+// const vardas = 'Iveastas vardas';
+// const pavarde = 'Ivesta pavarde';
+
+
+// --- IVESTAS TEKSTAS PO !PASPAUDIMO! I JAVASCRIPT
+function ivesti() {
+
+    // --- IVESTAS VARDAS PAGAUNAMAS
+    const vardas = document.getElementById('vardas');
+
+    // --- IVESTAS VARDAS PAGAUNAMAS
+    const pavarde = document.getElementById('pavarde');
+
+}
+
+
+// --- IVESTAS TEKSTAS PERMETAMS I USER OBJEKTA
+const user = {
+    vardas,
+    pavarde,
+  };
+
+// --- IVESTA TEKSTA PERMETAME I STRINGA SU KITU TEKSTU
+ let printText= `<h2>User information</h2>
+ <p> Vartotojo vardas yra ${user.vardas}, o pavarde ${user.pavarde}</p>`
+
+
+// --- PARODOME IVESTA STRINGA HTML'E
+main.innerHTML=printText;
 
 
